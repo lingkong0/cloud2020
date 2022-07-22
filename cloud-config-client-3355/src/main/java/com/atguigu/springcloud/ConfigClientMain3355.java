2a154aeb61795d80199a18b69f86b85a9fe90f86
song <song_jiahai@libovito.com>
2022-07-22 15:49:37 +0800

    第一次提交

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -2,12 +2,14 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author:lilun
  * @create: 2022-07-22 15:06
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientMain3355 {
     public static void main(String[] args) {
         SpringApplication.run( ConfigClientMain3355.class,args);
